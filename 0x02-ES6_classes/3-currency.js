4e9c73b9911dbb887a7f8902e1f1da13ead6db26
beryl452 <berylhoe+alx@gmail.com>
2024-03-07 15:10:58 +0000

    Fix 4-pricing.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -36,6 +36,6 @@ export default class Currency {
   }
 
   displayFullCurrency() {
-    return `${this.name} (${this.code})`;
+    return `${this._name} (${this._code})`;
   }
 }
---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,43 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!currency instanceof Currency) {
+      throw new TypeError('Currency must be instance of Currency');
+    }
+    
+    this.amount = amount;
+    this.currency = currency;
+  }
+  
+  get amount() {
+    return this._amount;
+  }
+
+  get currenry() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else {
+      this._amount = amount;
+    }
+  }
+
+  set currency(currency) {
+    if (!currency instanceof Currency) {
+      throw new TypeError('Currency must be instance of Currency');
+    } else {
+      this._currency = currency;
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+}
