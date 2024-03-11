d4a6a95cc852b88b544b16f1d4fbb04363d6b19f
beryl452 <berylhoe+alx@gmail.com>
2024-03-11 15:56:09 +0000

    Fix linter error in task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,9 +2,8 @@ export default function getStudentsByLocation(listStudent, city) {
   if (listStudent instanceof Array) {
     if (typeof city === 'string') {
       return listStudent.filter((student) => student.location === city);
-    } else {
-      return listStudent;
     }
+    return listStudent;
   }
   return [];
 }
