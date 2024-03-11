02456071b96b02753093cc087f4cb483b01d6ec9
beryl452 <berylhoe+alx@gmail.com>
2024-03-11 09:07:30 +0000

    End of 3-get_ids_sum.js

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,6 @@
+export default function getStudentIdsSum(listStudent) {
+  if (listStudent instanceof Array) {
+    return listStudent.reduce((acc, student) => acc + student.id, 0);
+  }
+  return []
+}
