7e4ed430a99e4429720cf269008efdcf61bca956
beryl452 <berylhoe+alx@gmail.com>
2024-03-12 11:29:55 +0000

    Fix 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (!set || !startString) {
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
-  if (!(set instanceof Set)) {
-    throw new TypeError('set must be instance of Set');
-  }
-  if (typeof startString !== 'string') {
-    throw new TypeError('startString must be string');
-  }
+
   const result = [];
   for (const item of set) {
     if (item.startsWith(startString)) {
