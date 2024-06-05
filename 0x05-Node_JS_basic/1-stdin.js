d684a541ab7c04b70e1ee4d3dcc3ba26f0c28133
HOUESSOU Béryl <beryl.houessou@gmail.com>
2024-06-05 01:12:01 +0000

    Fix linter error

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', function() {
+process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
   if (chunk !== null) {
@@ -8,6 +8,6 @@ process.stdin.on('readable', function() {
   }
 });
 
-process.stdin.on('end', function() {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
